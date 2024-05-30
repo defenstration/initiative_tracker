b19c888118c788170f5195f446b137740c9af3ed
Blackman <DBlackman@int.co.chenango.ny.us>
2024-05-30 14:40:45 -0400

    Add basic button functionality

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const addPlayerBtn = document.getElementById('add-player-btn')
 const addEnemyBtn = document.getElementById('add-enemy-btn')
+const playerEntryForm = document.getElementById("player-entry-form")
+const playerSubmitBtn = document.getElementById("player-submit-btn")
+const enemyEntryForm = document.getElementById("enemy-entry-form")
+const enemySubmitBtn = document.getElementById("enemy-submit-btn")
 
 addPlayerBtn.addEventListener("click", () => {
-    console.log("clicked")
+    playerEntryForm.style.display = "block"
+
+})
+
+addEnemyBtn.addEventListener("click", () => {
+    enemyEntryForm.style.display = "block"
+})
+
+playerSubmitBtn.addEventListener("click", () => {
+    playerEntryForm.style.display = "none"
+})
+
+enemySubmitBtn.addEventListener("click", () => {
+    enemyEntryForm.style.display = "none"
 })
 
