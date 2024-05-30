d37f3ffd442dfb7ed2af775c462e2b8832ab764c
Blackman <DBlackman@int.co.chenango.ny.us>
2024-05-30 14:19:12 -0400

    Add start of player entry and player entry script

---
+++ script.js
@@ -0,0 +1,6 @@
+const addPlayerBtn = document.getElementById(add-player-btn)
+const addEnemyBtn = document.getElementById(add-enemy-btn)
+
+addPlayerBtn.addEventListener("click", () => {
+    console.log("clicked")
+})
